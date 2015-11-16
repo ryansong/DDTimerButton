c59d929df3f2ddf3f4c49d452d9b89a2153a8fec
ryansong <song.ryan90@gmail.com>
2015-11-16 13:16:31 +0800

    实现验证码按钮

--- DDTimerButton/ViewController.m
+++ DDTimerButton/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "DDTimerButton.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic)  DDTimerButton *b1;
 
 @end
 
@@ -16,7 +19,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _b1 = [[DDTimerButton alloc] initWithInterval:60 title:@"发送验证码" withIdentifier:@"code"];
+    _b1.sendVerifyCodeBlock = ^{return YES;};
+    
+    __weak ViewController *weakSelf = self;
+    _b1.finishVerifyBlock = ^{
+        [weakSelf.b1 setTitle:@"重新发送" forState:UIControlStateNormal];
+    };
+    
+    [_b1 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    
+    [_b1 sizeToFit];
+    
+    _b1.frame = CGRectMake(100, 200, 200, 32);
+    
+    [self.view addSubview:self.b1];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
